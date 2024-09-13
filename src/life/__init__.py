494139b5201415ecf4f25c784c8d63612350ba16
jazib <jazibcheema5@gmail.com>
2024-09-13 15:18:53 +0200

    Added the License and changed the license program to my project description

---
+++ src/life/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about life."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Jazib Imran"
+__email__ = "jazibcheema5@gmail.com"
+__version__ = "0.1.0"
